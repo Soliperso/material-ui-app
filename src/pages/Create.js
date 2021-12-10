3ceb9f1f87ba20dd1af437aa25f3ca241ba6dc73
Soliperso <chah762002@yahoo.fr>
2021-12-09 17:11:01 -0800

    add custom css (makeStyles Hook)

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,13 +1,44 @@
 import { Button, Container, Typography } from '@mui/material';
-import { SendOutlinedIcon } from '@mui/icons-material';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import { makeStyles } from '@mui/styles';
+
+// Create custom CSS styles using makeStyles hook
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 40,
+    backgroundColor: 'violet',
+
+    '&:hover': {
+      backgroundColor: '#00ff00',
+    },
+  },
+  title: {
+    fontSize: 40,
+    textDecoration: 'underline',
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
+
   return (
     <Container>
-      <Typography variant='h6' color='textSecondary' component='h2' gutterBottom>
+      <Typography
+        className={classes.title}
+        variant='h6'
+        color='textSecondary'
+        component='h2'
+        gutterBottom
+      >
         Create a New Note
       </Typography>
-      <Button type='submit' variant='contained' color='secondary' endIcon={<SendOutlinedIcon />}>
+      <Button
+        type='submit'
+        className={classes.btn}
+        variant='contained'
+        color='secondary'
+        endIcon={<SendOutlinedIcon />}
+      >
         Submit
       </Button>
     </Container>
