8310e55cb942d7fa2b083dd8d7d0d365a4e1aaf3
Soliperso <chah762002@yahoo.fr>
2021-12-09 17:22:17 -0800

    add custom theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Notes from './pages/Notes';
 import Create from './pages/Create';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { cyan } from '@mui/material/colors';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#00bcd4',
+    },
+    secondary: cyan,
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+  },
+});
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Notes />} />
-        <Route path='/create' element={<Create />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path='/' element={<Notes />} />
+          <Route path='/create' element={<Create />} />
+        </Routes>
+      </ThemeProvider>
     </Router>
   );
 }
