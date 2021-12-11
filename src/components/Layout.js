f071fe9bd35c290c3fc14b8d0aabab69a75797ff
Soliperso <chah762002@yahoo.fr>
2021-12-10 16:33:34 -0800

    add Permanent Drawer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { makeStyles } from '@mui/styles';
+import { Drawer, Typography } from '@mui/material';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
   page: {
     background: '#f9f9f9',
     width: '100%',
-    height: '10vh',
+    height: '100vh',
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: 'flex',
   },
 });
 
@@ -13,7 +25,15 @@ const Layout = ({ children }) => {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
+      <Drawer
+        variant='permanent'
+        className={classes.drawer}
+        anchor='left'
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <Typography variant='h6'>Ed's Notes</Typography>
+      </Drawer>
       <div className={classes.page}>{children}</div>
     </div>
   );
